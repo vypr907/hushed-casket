fd2867c9c749895d4f2fbadde08782a97a2f1fb3
vypr907 <steven.laszloffy@gmail.com>
2025-08-08 15:31:54 -0700

    fixed local_host variable usage and definitions

--- dc-deploy/main.tf
+++ dc-deploy/main.tf
@@ -23,6 +23,7 @@ module "vsphere_vm_dc" {
   datacenter             = var.datacenter
   datastore              = var.datastore
   host                   = var.host
+  local_host             = var.local_host
   network                = var.network
   vm_name                = var.vm_name
   iso_path               = var.iso_path
